2b4993d281280a452e32d90ff003c907cb3e67b9
Chisom Chukwuma <chukwumachisom18@gmail.com>
2022-03-18 12:06:12 -1100

    movies search app

--- src/App.js
+++ src/App.js
@@ -1,44 +1,81 @@
 import { useState, useEffect, Fragment } from 'react'
 import axios from 'axios'
 import './style/All.css'
-import patternMobile from './assets/pattern-divider-mobile.svg'
-import patternDesktop from './assets/pattern-divider-desktop.svg'
-import dice from './assets/icon-dice.svg'
+
 const App = () => {
-  const [advice, setAdvice] = useState([])
+  const [movie, setMovie] = useState([])
+  const [search, setSearch] = useState('')
   const [loading, setLoading] = useState(true)
 
-  const getAdvice = () => {
-    axios.get('https://api.adviceslip.com/advice').then((response) => {
-      console.log(response)
-      const myAdvice = response.data
-      setAdvice(myAdvice)
-      setLoading(false)
-      console.log(advice);
-    })
+  const getMovie = () => {
+    axios
+      .get('http://www.omdbapi.com/?i=tt3896198&apikey=ec44a943')
+      .then((response) => {
+        // console.log(response)
+        const myMovie = response.data
+        setMovie(myMovie)
+        setLoading(false)
+      })
   }
   useEffect(() => {
-    getAdvice()
+    getMovie()
   }, [])
-  // console.log(advice.slip.id);
+
+  const movies = []
+  movies.push(movie)
+  console.log(movies)
+
   return (
     <div className='App'>
+      <div className='first-div'>
+        <div className='second-div text-capitalize text-center'>
+          <p className='mt-1 mt-md-3'>myTestApp</p>
+        </div>
+      </div>
+      <div className='third-div text-center'>
+        <p>Watch something incredible.</p>
+      </div>
       {loading === false && (
         <Fragment>
-          <div className='small'>
-            <small> advice #{advice.slip.id}</small>
-          </div>
-          <div className='lorem'>
-            <p>
-              {advice.slip.advice}
-            </p>
-          </div>
-          <div className='pause'>
-            <source media='(min-width:768px)' srcSet={patternDesktop} />
-            <img src={patternMobile} alt='divider' />
-          </div>
-          <div className='dice'>
-            <img onClick={getAdvice} src={dice} alt='Dice' />
+          <div className='fourth-div p-4'>
+            <p className='search ml-md-5'>Search</p>
+            <input
+              type='text'
+              id='search'
+              className='input ml-md-5'
+              onChange={(event) => {
+                setSearch(event.target.value)
+              }}
+            />
+            {movies
+              .filter((item) => {
+                if (search == '') {
+                  return null
+                } else if (
+                  item.Title.toLowerCase().includes(search.toLowerCase())
+                ) {
+                  return item
+                }
+              })
+              .map((item, key) => {
+                return (
+                  <div key={key}>
+                    <div className='list'>
+                      {item.Title}
+                    </div>
+                  </div>
+                )
+              })}
+            {movies.map((item, key) => {
+              return (
+                <div key={key}>
+                  <div className='movie-genre ml-md-5 mt-md-5'>{item.Genre}</div>
+                  <div className='movie-name mt-5 ml-md-5'>
+                    <p>{item.Title === search ? search : item.Title}</p>
+                  </div>
+                </div>
+              )
+            })}
           </div>
         </Fragment>
       )}
